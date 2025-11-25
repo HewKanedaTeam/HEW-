6b05d48693201244ed2d49c02a05e34cb55b2ef5
yuuki <yu04ki23123@gmail.com>
2025-11-25 17:38:33 +0900

    Update Character.h

--- Character.h
+++ Character.h
@@ -2,6 +2,14 @@
 #include "Object.h"
 class Character : public Object
 {
+protected:
+	int hp;
+	bool isGround;
+	bool isNotDamage;
 
+public:
+	virtual void Move()=0;
+	virtual void HitCheck()=0;
+	virtual void Attack()=0;
+	virtual void Jump()=0;
 };
-
