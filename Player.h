c9a2697b446ff4a463bf3061e02d83910fbcfc22
higashino-haruka <h.haruka0514@gmail.com>
2025-11-26 11:53:20 +0900

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,1 +1,40 @@
 #pragma once
+#include"Character.h"
+
+enum PlayerState
+{
+	STY,
+	BRINK,
+	JUMP,
+	NODAMAGE,
+	STAN,WALL_GRAB,
+	ATTACK,
+	RIGHT,
+	LIGHT,
+
+};
+
+class Player : public Character
+{
+	protected:
+		int hp;
+		bool isGround;
+
+	private:
+		bool isJump;
+		bool isBlink;
+		bool isHit;
+		bool isWall;
+		bool isWallJump;
+		bool isDodge;
+
+	public:
+		void Move();
+		void HitCheck();
+		void Attack();
+		void Blink();
+		void Jump();
+		void WallJump();
+		void GetBlink();
+};
+
